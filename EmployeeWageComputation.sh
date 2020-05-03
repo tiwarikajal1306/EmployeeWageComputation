f30ba20f0a84b13adf1213330077a4a1a26e134b
kajal <tiwarikajal1306@gmail.com>
2020-05-03 18:54:28 +0530

    Add part time employee and wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -x
 echo "Welcome msg"
-ispresent=1;
-randomCheck=$((RANDOM%2));
-if [ $ispresent -eq $randomCheck ]
-then
-      salaryperHour=20;
-      workingHourperday=8;
-      salary=$(($salaryperHour * $workingHourperday))
-else
-     salary=0;
-fi
-echo $salary;
+empCheck=$((RANDOM % 3))
+isPartTime=1
+isFullTime=2
+salaryPerHr=20
 
+if [ $isPartTime -eq $empCheck ]
+then
+	echo "Present for part time"
+	workHrs=4
+elif [ $isFullTime -eq $empCheck ]
+then
+	echo "Present for Full Time"
+	workHrs=8
+else
+	echo "Absent"
+fi
+
+salary=$((workHrs * salaryPerHr))
+echo "Salary: "$salary
 
 
 
