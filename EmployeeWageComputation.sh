0ebdd054a9cde8bf9303475f70c3e6f0f772292b
kajal <tiwarikajal1306@gmail.com>
2020-05-04 10:51:17 +0530

    Store the daily wage along wih the total wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -26,14 +26,21 @@ function getWorkHrs() {
         echo $empHrs
 }
 
+function getEmpWage() {
+	local empHr=$1
+	echo $((empHr * EMP_RATE_PER_HR))
+}
+
 while [ $totalEmpHr -lt $MAX_HRS_IN_MONTH -a $totalWorkingDays -lt $NUM_WORKING_DAYS ]
 do
         ((totalWorkingDays++))
         empCheck=$((RANDOM % 3))
         empHrs="$( getWorkHrs $empCheck )"
-        totalEmpHr=$(($totalEmpHr+$empHrs))
+        totalEmpHr=$(($totalEmpHr + $empHrs))
+	dailyWage[$totalWorkingDays]="$( getEmpWage $empHrs )"
 done
 
+echo ${dailyWage[@]}
 totalSalary=$((totalEmpHr*$EMP_RATE_PER_HR));
 echo "Total Employee Hours: " $totalEmpHr;
 echo "Total Employee salary: "$totalSalary;
